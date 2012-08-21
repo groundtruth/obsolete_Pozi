63e17417ba2ca3b89fbf66aceee4df92ba001341
Herve Senot <hsenot@gmail.com>
2012-08-21 15:56:42 +1000

    Service returning a property address JSON instead of a property

--- ws/rest/v3/ws_property_id_by_propnum.php
+++ ws/rest/v3/ws_property_id_by_propnum.php
@@ -20,6 +20,7 @@ $err_email = "herve.senot@groundtruth.com.au";
 # Retrive URL arguments
 try {
 	$query = $_REQUEST['query'];
+	$lga = $_REQUEST['lga'];
 	$format = 'json';
 }
 catch (Exception $e) {
@@ -28,7 +29,11 @@ catch (Exception $e) {
 
 # Performs the query and returns XML or JSON
 try {
-	$sql = sanitizeSQL("select * from gt_master_search where gsln='VMPROP_PROPERTY' and idval='".$query."' limit 1");
+//	$sql = sanitizeSQL("select * from gt_master_search where gsln='VMPROP_PROPERTY' and idval='".$query."' and lga='".$lga."' limit 1");
+//	$sql = sanitizeSQL("select * from gt_master_search s where s.gsln='VICMAP_PROPERTY_ADDRESS' and s.idval=(select p.ezi_add from dse_property_address p where p.pr_propnum='".$query."' and p.lga_code='".$lga."' limit 1) and s.lga='".$lga."' limit 1");
+//	$sql = sanitizeSQL("select label,(select min(st_x) from (select ST_X(ST_PointN(bbox,generate_series(1, bbox_length))) as st_x) xm) as xmini,(select min(st_y) from (select ST_Y(ST_PointN(bbox,generate_series(1, bbox_length))) as st_y) ym) as ymini,(select max(st_x) from (select ST_X(ST_PointN(bbox,generate_series(1, bbox_length))) as st_x) xm2) as xmaxi,(select max(st_y) from (select ST_Y(ST_PointN(bbox,generate_series(1, bbox_length))) as st_y) ym2) as ymaxi,'VICMAP' as gsns,'VICMAP_PROPERTY_ADDRESS' as gsln,'ezi_add' as idcol,ezi_add as idval,'Address' as ld,lga from (select ezi_add,upper(ezi_add) as label,ST_Boundary(ST_Envelope(the_geom)) as bbox,ST_NPoints(ST_Boundary(ST_Envelope(the_geom)))-2 as bbox_length,lga from (select ezi_add,lga_code as lga,ST_Union(the_geom) as the_geom from dse_property_address where pr_propnum='".$query."' and lga_code='".$lga."' group by ezi_add,lga_code) u) t");
+	$sql = sanitizeSQL("select label,(select min(st_x) from (select ST_X(ST_PointN(bbox,generate_series(1, bbox_length))) as st_x) xm) as xmini,(select min(st_y) from (select ST_Y(ST_PointN(bbox,generate_series(1, bbox_length))) as st_y) ym) as ymini,(select max(st_x) from (select ST_X(ST_PointN(bbox,generate_series(1, bbox_length))) as st_x) xm2) as xmaxi,(select max(st_y) from (select ST_Y(ST_PointN(bbox,generate_series(1, bbox_length))) as st_y) ym2) as ymaxi,'VICMAP' as gsns,'VICMAP_PROPERTY_ADDRESS' as gsln,'pr_propnum' as idcol,'".$query."' as idval,'Address' as ld,lga from (select ezi_add,upper(ezi_add) as label,ST_Boundary(ST_Envelope(the_geom)) as bbox,ST_NPoints(ST_Boundary(ST_Envelope(the_geom)))-2 as bbox_length,lga from (select ezi_add,lga_code as lga,ST_Union(the_geom) as the_geom from dse_property_address where pr_propnum='".$query."' and lga_code='".$lga."' group by ezi_add,lga_code) u) t");
+
 	$pgconn = pgConnection();
 
     /*** fetch into an PDOStatement object ***/
